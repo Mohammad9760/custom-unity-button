d8b58d428564000bfec0e94d4fee9316b1c8608f
Mohammad9760 <79392031+Mohammad9760@users.noreply.github.com>
2021-02-23 22:00:26 -0800

    Update Btn.cs

--- Btn.cs
+++ Btn.cs
@@ -1,46 +1,69 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class Btn : MonoBehaviour , IPointerClickHandler, IPointerDownHandler
+public class Btn : MonoBehaviour ,IPointerDownHandler , IPointerUpHandler, IPointerClickHandler
 {
-    public bool Clicked, DoubleClicked;
+    [HideInInspector]
+	public bool Held = false ,Clicked = false , doubleClicked = false;
+    [SerializeField]
+    private float delay = 0.7f;
 
-    private float doubleclickDelay = 0.5f, passedTimeSinceLastClick;
 
-    void Start()
+    [SerializeField]
+    Color color , pressedColor = new Color(200,200,200,255);
+    private Image image;
+    private Text btnText;
+    private void Start()
     {
-        
+        image = GetComponent<Image>();
+        btnText = transform.Find("Text").GetComponent<Text>();
+        color = image.color;
+        pressedColor = color * pressedColor;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        passedTimeSinceLastClick += Time.deltaTime;
-    }
+	private void Update () 
+	{
+		delay -= (Time.deltaTime);
+
+        if (Held)
+            image.color = pressedColor;
+        else image.color = color;
+
+	}
+
+    /// <summary>
+    /// use this to change the button's text with no need to refrencing the text gameobject
+    /// </summary>
+    public void ChangeText(string newText) { btnText.text = newText; }
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		Held = true;
+		if (delay > 0){
+			doubleClicked = true;
+            StartCoroutine(reset());
+        }
+		else
+			delay = 0.7f;
+	}
+
+	public void OnPointerUp(PointerEventData eventData) {	Held = false;	}
+
+    public void OnPointerClick(PointerEventData eventData) {   Clicked = true; StartCoroutine(reset());}
 
     private IEnumerator reset()
     {
-        yield return new WaitForEndOfFrame();
-        Clicked = false;
-        DoubleClicked = false;
-    }
-
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        Clicked = true;
-        StartCoroutine(reset());
-    }
-
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        if (passedTimeSinceLastClick < doubleclickDelay)
+        if (Clicked)
         {
-            DoubleClicked = true;
-            StartCoroutine(reset());
+            yield return new WaitForEndOfFrame();
+            Clicked = false;
         }
-        else
-            passedTimeSinceLastClick = 0;
+        if (doubleClicked)
+        {
+            yield return new WaitForEndOfFrame();
+            doubleClicked = false;
+        }
     }
 }
